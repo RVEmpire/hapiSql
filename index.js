5a89bd3882df19c46ef1d3e75b11d66097116835
rahulteja16 <rahulteja164@gmail.com>
2016-12-29 21:40:04 +0530

    Added Mysql Connections

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 'use strict';
 
 var Hapi = require('hapi');
+var mysql = require('mysql');
 var server = new Hapi.Server();
 
 server.connection({ port : 9000 });
+
+var mysqlConnection = mysql.createConnection({
+	host : 'localhost',
+	user : 'root',
+	password : '1234',
+	database: 'test'
+});
+
+mysqlConnection.connect();
+
+mysqlConnection.query('select * from user', function(err, rows, fields){
+	console.log('Name: ' + rows[0].name + ' Email: ' + rows[0].email);
+});
 
 server.route({
 	method: 'GET',
@@ -12,6 +26,8 @@ server.route({
 		reply('Its Working');
 	}
 });
+
+mysqlConnection.end();
 
 server.start(function(){
 	console.log('server is running!');
