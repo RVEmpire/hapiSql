038f0968fe7b7a0b507410e9d6dcaec4fd98803a
Vijay SIngh <vj2010sn@gmail.com>
2017-01-11 23:32:05 +0530

    User model base (#8)
    * user model refined
    
    * user model refined and code refractor

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ var models = require('../models');
 module.exports = {
 	get:function (request, reply) {
 		models.users.findAll()
-		.then(function(data){
+		.then(function(data) {
 			if(data){
 				return reply(null, {message: 'Data has been fetched!', success: true, data: data})
 			} else {
@@ -12,73 +12,70 @@ module.exports = {
 		})
 		.catch(function(error){
 			return reply(null, {message: 'failed to connect to database', success: false});
-		})
-		
+		});
 	},
 	insert:function (request, reply) {
 		var userEmail = request.payload.email;
 		var userName = request.payload.name;
-		var userUrl = request.payload.email;
-	  	models.users.create({
-	  		email: userEmail,
-	  		name: userName,
-	  		url: userUrl
-	  	})
-	  	.then(function(data){
-	  		if(data.dataValues){
-	  			return reply(null, {message: 'Data has been inseted!', success: true, data: data.dataValues})
-	  		} else {
-	  			return reply(null, {message:'Failed to insert', success: false});
-	  		}
-	  	})
-	  	.catch(function(error){
-	  		return reply(null, {message: 'failed to connect to database', success: false});
-	  	})
+
+  	models.users.create({
+  		email: userEmail,
+  		name: userName,
+  	})
+  	.then(function(data) {
+  		if (data.dataValues) {
+  			return reply(null, { message: 'Data has been inseted!', success: true, data: data.dataValues });
+  		} else {
+  			return reply(null, { message:'Failed to insert', success: false });
+  		}
+  	})
+  	.catch(function(error) {
+  		return reply(null, {message: 'failed to connect to database', success: false});
+  	});
 	},
 	delete: function(request, reply){
 		var userEmail = request.payload.email;
+
 		models.users.destroy({
 			where:{
 				email: userEmail
 			}
 		})
-		.then(function(data){
+		.then(function(data) {
 			if(data){
-				return reply(null, {message: 'Data has been deleted!', success: true})
+				return reply(null, { message: 'Data has been deleted!', success: true });
 			} else {
-				return reply(null, {message:'Failed to delete', success: false});
+				return reply(null, { message:'Failed to delete', success: false });
 			}
 		})
-		.catch(function(error){
-			return reply(null, {message: 'failed to connect to database', success: false});
-		})
+		.catch(function(error) {
+			return reply(null, { message: 'failed to connect to database', success: false}  );
+		});
 	},
 	update:function(request, reply) {
 		var userEmail = request.payload.email;
 		var userName = request.payload.name;
-		var userUrl = request.payload.url;
 		models.users.update(
 		{
 			name : userName,
-			url : userUrl
 		},
 		{
-			fields: ['name','url'],
+			fields: ['name'],
 			validate: true,
-			where: 
+			where:
 			{
 				email: userEmail
 			}
 		})
-		.then(function(data){
+		.then(function(data) {
 			if(data == 1) {
-				return reply(null, {message:'Updated Succeefully', success: true});
+				return reply(null, { message:'Updated Succeefully', success: true });
 			} else {
 				return reply(null, {message:'Failed to update', success: false});
 			}
 		})
-		.catch(function(error){
-			return reply(null, {message: 'failed to connect to database', success: false});
-		})
+		.catch(function(error) {
+			return reply(null, { message: 'failed to connect to database', success: false });
+		});
 	}
 };
