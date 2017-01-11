2eb06edf49729ef73ac87f2366f49fc6714f6687
vijay singh <vj2010sn@gmail.com>
2017-01-11 23:21:18 +0530

    user model refined

--- models/users.js
+++ models/users.js
@@ -1,15 +1,32 @@
-'use strict';
+/* jshint indent: 2 */
+
 module.exports = function(sequelize, DataTypes) {
-  var users = sequelize.define('users', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    url: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
+  return sequelize.define('users', {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.TIME,
+      allowNull: true,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+      type: DataTypes.TIME,
+      allowNull: true,
+      defaultValue: "0000-00-00 00:00:00"
     }
+  }, {
+    tableName: 'users'
   });
-  return users;
 };
