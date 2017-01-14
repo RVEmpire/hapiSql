b588f36b7e8c2930b820d3ec4f54a3ed11d2276b
Vijay SIngh <vj2010sn@gmail.com>
2017-01-14 22:40:02 +0530

    Phase I continued (#10)
    * added chalk and added team logo
    
    * Update ReadMe
    
    * code refractor, good console added, chalk implemented as utility and logo added

--- server.js
+++ server.js
@@ -3,9 +3,15 @@
 var Hapi = require('hapi');
 var path = require('path');
 var settings = require('config');
+var chalk = require('chalk');
+var Good = require('good');
+
+//Custome lib
+var $ = require('./lib/index');
 
 //routes
 var routes = require('./routes');
+
 //models
 var models = require('./models');
 
@@ -17,7 +23,11 @@ var server = new Hapi.Server({
   }
 });
 
-server.connection({ port:settings.port,  host:settings.host});
+server.connection({ port: settings.port,  host: settings.host });
+
+server.on('response', function (request) {
+  $.log.info(request.info.remoteAddress + ': ' + request.method.toUpperCase() + ' ' + request.url.path + ' --> ' + request.response.statusCode);
+});
 
 module.exports = server;
 
@@ -36,11 +46,43 @@ var setup = function(done){
 };
 
 var start = function(){
-  server.start(function(err){
-    if (err) {
-      console.log("err", err);
+  // register plugins to server instance
+  server.register({
+    register: Good,
+    options: {
+      ops : false,
+      reporters: {
+        reporter: [
+          {
+            module: 'good-console',
+            args: [{ response: '*', log: '*'}]
+          }, 'stdout'
+        ]
+      }
     }
-    console.log('server running at: ' + server.info.uri);
+  },
+  function (err) {
+    if (err) {
+      $.log.error(err);
+      process.exit(1);
+    }
+    server.start(function(err){
+      if (err) {
+        $.log.error(err);
+        process.exit(1);
+      }
+      console.log(chalk.yellow("#####################################################################################################\n"+
+                               "###      ########      ##     ##        ######## ##     ## ########  #### ########  ########      ###\n"+
+                               "###      ##     ##     ##     ##        ##       ###   ### ##     ##  ##  ##     ## ##            ###\n"+
+                               "###      ##     ##     ##     ##        ##       #### #### ##     ##  ##  ##     ## ##            ###\n"+
+                               "###      ########      ##     ##        ######   ## ### ## ########   ##  ########  ######        ###\n"+
+                               "###      ##   ##        ##   ##         ##       ##     ## ##         ##  ##   ##   ##            ###\n"+
+                               "###      ##    ##  ###   ## ##   ###    ##       ##     ## ##         ##  ##    ##  ##            ###\n"+
+                               "###      ##     ## ###    ###    ###    ######## ##     ## ##        #### ##     ## ########      ###\n"+
+                               "#####################################################################################################\n"));
+
+      $.log.info('server running at: ' + server.info.uri);
+    });
   });
 };
 
