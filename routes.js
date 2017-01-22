abc5967a50a0f4101e625b1af2ac26283527cfb9
vijay singh <vj2010sn@gmail.com>
2017-01-22 23:12:35 +0530

    hapi auth cookie integrated

--- routes.js
+++ routes.js
@@ -1,23 +1,44 @@
-'use strict';
+"use strict";
 
-var controllers = require('./controllers');
-var Joi = require('joi');
+var controllers = require("./controllers");
+var Joi = require("joi");
 
 module.exports = [
   {
-    method: 'GET',
-    path: '/',
+    method: "GET",
+    path: "/",
     handler: controllers.home.default
   },
   {
-    method: 'GET',
-    path: '/users/get',
-    handler: controllers.users.get
+    method: "GET",
+    path: "/users/get",
+    config: {
+      auth: {
+        mode: "try",
+        strategy: "session"
+      },
+      handler: controllers.users.get
+    }
   },
   {
-    method: 'POST',
-    path: '/users/update',
-    handler: controllers.users.update,
+    method: "POST",
+    path: "/users/update",
+    config: {
+      validate: {
+        payload: {
+          name: Joi.string().min(6).max(50).required(),
+        }
+      },
+      auth: {
+        mode: "try",
+        strategy: "session"
+      },
+      handler: controllers.users.update
+    }
+  },
+  {
+    method: "POST",
+    path: "/users/insert",
     config: {
       validate: {
         payload: {
@@ -25,46 +46,51 @@ module.exports = [
           name: Joi.string().min(6).max(50).required(),
           password: Joi.string().min(6).max(20).required()
         }
-      }
+      },
+      auth: {
+        mode: "try",
+        strategy: "session"
+      },
+      handler: controllers.users.insert
     }
   },
   {
-    method: 'POST',
-    path: '/users/insert',
-    handler: controllers.users.insert,
+    method: "DELETE",
+    path: "/users/delete",
     config: {
-      validate: {
-        payload: {
-          email: Joi.string().email().required(),
-          name: Joi.string().min(6).max(50).required(),
-          password: Joi.string().min(6).max(20).required()
-        }
-      }
+      auth: {
+        mode: "try",
+        strategy: "session"
+      },
+      handler: controllers.users.delete
     }
   },
   {
-    method: 'POST',
-    path: '/users/delete',
-    handler: controllers.users.delete,
-    config: {
-      validate: {
-        payload: {
-          email: Joi.string().email().required(),
-        }
-      }
-    }
-  },
-  {
-    method: 'POST',
-    path: '/users/login',
-    handler: controllers.users.login,
+    method: "POST",
+    path: "/users/login",
     config: {
       validate: {
         payload: {
           email: Joi.string().email().required(),
           password: Joi.string().min(6).max(20).required()
         }
-      }
+      },
+      auth: {
+        mode: "try",
+        strategy: "session"
+      },
+      handler: controllers.users.login
+    }
+  },
+  {
+    method: "GET",
+    path: "/users/logout",
+    config: {
+      auth: {
+        mode: "try",
+        strategy: "session"
+      },
+      handler: controllers.users.logout
     }
   }
 ];
